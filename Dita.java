9a0474a03b964772f0be8b75960226d79525b8a3
DitaLasmanasari <71079482+DitaLasmanasari@users.noreply.github.com>
2020-09-11 08:38:00 +0700

    Add files via upload

---
+++ Dita.java
@@ -0,0 +1,33 @@
+
+package utsdita;
+
+public class Dita {
+    public static void main (String[] args ){
+        String var1 =" ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char[] c = {var1.charAt(4),
+        var1.charAt(9),
+        var1.charAt(20),
+        var1.charAt(1),
+        var1.charAt(0),
+        var1.charAt(12),
+        var1.charAt(1),
+        var1.charAt(19),
+        var1.charAt(13),
+        var1.charAt(1),
+        var1.charAt(14),
+        var1.charAt(1),
+        var1.charAt(19),
+        var1.charAt(1),
+        var1.charAt(18),
+        var1.charAt(9),
+        };
+                
+        for (int i=0;i<c.length;i++){
+        System.out.print(c[i]);
+        
+        }
+        System.out.println();
+
+    }
+        
+}
